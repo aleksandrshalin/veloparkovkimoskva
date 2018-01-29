0b3800ac076ed048dd42440759b45ddcbe90d0e7
Sasha <a.shalin@me.com>
2018-01-29 12:30:12 +0300

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SOURCE_DIR=`dirname "${BASH_SOURCE[0]}"` #Узнаем путь до папки исполняемого файла
+SOURCE_DIR=`dirname "${BASH_SOURCE[0]}"` 
 
-wget https://apidata.mos.ru/v1/datasets/916/features?api_key=3a6cb7b16a2e081b663ad089f4174fc6 -O "$SOURCE_DIR/"mos_data.json #Скачиваем данные
+wget https://apidata.mos.ru/v1/datasets/916/features?api_key=3a6cb7b16a2e081b663ad089f4174fc6 -O "$SOURCE_DIR/"mos_data.json 
